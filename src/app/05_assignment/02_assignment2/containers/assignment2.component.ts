e73fc47d6994dbb0ed5a293208bdab6d359f8ec5
Aakansha-20 <aakansha.bhardwaj2000@gmail.com>
2020-04-21 23:17:44 +0530

    'master'

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
-
+import { Component,Input } from '@angular/core';
+import { Todo } from 'src/app/03_pipes/models/todo.interface';
 @Component({
   selector: 'app-assignment2',
   templateUrl: './assignment2.component.html',
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component {
+  @Input()
+  todoItems: Todo[];
+ 
+  @Input()
+  selectedItem: Todo;
+ 
 
+  onSelectTodo(todo) {
+
+  this.selectedItem=todo;
+  }
 }
